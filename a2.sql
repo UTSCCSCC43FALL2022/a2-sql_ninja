cd4c19500350b7378e07a4ac03bc8f005d304315
SimonLi1020 <55788824+SimonLi1020@users.noreply.github.com>
2022-11-01 10:51:21 -0400

    upload starter files

---
+++ a2.sql
@@ -0,0 +1,36 @@
+SET search_path TO A2;
+
+-- If you define any views for a question (you are encouraged to), you must drop them
+-- after you have populated the answer table for that question.
+-- Good Luck!
+
+-- Query 1 --------------------------------------------------
+INSERT INTO Query1
+
+-- Query 2 --------------------------------------------------
+INSERT INTO Query2
+
+-- Query 3 --------------------------------------------------
+INSERT INTO Query3
+
+-- Query 4 --------------------------------------------------
+INSERT INTO Query4
+
+-- Query 5 --------------------------------------------------
+INSERT INTO Query5
+
+-- Query 6 --------------------------------------------------
+INSERT INTO Query6
+
+-- Query 7 --------------------------------------------------
+INSERT INTO Query7
+
+-- Query 8 --------------------------------------------------
+INSERT INTO Query8
+
+-- Query 9 --------------------------------------------------
+INSERT INTO Query9
+
+-- Query 10 --------------------------------------------------
+INSERT INTO Query10
+
